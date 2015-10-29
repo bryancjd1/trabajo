d541f7cf65aeaaccd80bd0c370beaa6c392c43c2
bryancjd1 <bryan_cjd1@hotmail.com>
2015-10-29 01:25:41 -0500

    ...

--- Vehiculo/car.h
+++ Vehiculo/car.h
@@ -4,5 +4,7 @@ string style ;
 public :
     Car( const string & myLicense , const int myYear , const string & myStyle )
         : Vehicle ( myLicense , myYear ), style ( myStyle ) {}
+    const string getDesc ()
+        {return stringify ( year ) + ’ ’ + style + ": " + license ;}
     const string & getStyle () { return style ;}
 };
